c1a4f64137beb1aa276cc74518534832201fc784
amilford87 <amilford87@gmail.com>
2019-03-19 20:39:06 +0000

    exercise 9 complete

--- exercises/exercise_9.rb
+++ exercises/exercise_9.rb
@@ -13,3 +13,18 @@ puts "Exercise 9"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store1.destroy
+
+if Store.find(1)
+    puts "Could not destroy store :)"
+else
+    puts "Store destroyed! It has #{@store1.employees.size} =/"
+end
+
+@empty_store = Store.create!(name: "Test Empty Store", annual_revenue: 3, mens_apparel: true, womens_apparel: true)
+if @empty_store.destroy
+    puts "Empty Store destroyed!"
+else
+    puts "Whoa! Empty store should be destrpyable... Not cool!"
+end
--- lib/store.rb
+++ lib/store.rb
@@ -4,10 +4,18 @@ class Store < ActiveRecord::Base
     validates :annual_revenue, numericality: { only_integer: true }
     validates :annual_revenue, numericality: {greater_than: 0}
     validate :must_carry
-
+    
     def must_carry
         if !mens_apparel && !womens_apparel
             errors.add(name, "must have men or womens apparel")
+        end
+    end
+
+    def destroy
+        if employees.length > 0
+            
+        else
+            super
         end
     end
 
